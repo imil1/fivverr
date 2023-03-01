e9ba0f63fcf666e89e8a17e237422551db3e7fd3
imil <imil.net@mail.ru>
2023-03-01 15:38:03 +0500

    add deploy workflow

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './GigCard.scss';
 
-const GigCard = () => {
-  return (
-    <div className='gigCard'>
-        
-    </div>
-  )
-}
+const GigCard = ({ item }) => {
+    return (
+        <Link to='/gig/123'>
+            <div className='gigCard'>
+                <img
+                    src={item.img}
+                    alt=''
+                />
+                <div className='info'>
+                    <div className='user'>
+                        <img
+                            src={item.pp}
+                            alt='user'
+                        />
+                        <span>{item.username}</span>
+                    </div>
+                    <p>{item.desc}</p>
+                    <div className='star'>
+                        <img
+                            src='./img/star.png'
+                            alt='star'
+                        />
+                        <span>{item.star}</span>
+                    </div>
+                </div>
+                <hr />
+                <div className='details'>
+                    <img
+                        src='./img/heart.png'
+                        alt='heart'
+                    />
+                    <div className='price'>
+                        <span>starting at</span>
+                        <h2>${item.price}</h2>
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
+};
 
-export default GigCard
+export default GigCard;
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Gigs = () => {
   return (
-    <div>Gigs</div>
+    <div className='gigs'>
+      <div className='container'>
+        <span className='breadcrumbs'>Fiverr  GRAPHICS & DESIGN</span>
+        <h1>AI Artists</h1>
+        <p>EXPLORE THE BOUNDARIES OF ART</p>
+        <div className='menu'>
+          <div className='left'>
+            <span>Budged</span>
+            <input type='text' placeholder='min'/>
+            <input type='text' placeholder='max'/>
+            <button>Apply</button>
+          </div>
+          <div className='right'>
+            <span className='sortBy'>Sort by:</span>
+            <span className='sortType'>Best Selling</span>
+            <img src='./img/down.png' alt=''/>
+            <div className='rightMenu'>
+              <span>Newest</span>
+              <span>Best Selling</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
